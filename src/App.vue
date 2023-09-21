c5954a9d728c5adf75cdeaa0fd642fb3065a9781
Elisa Boscani <elisabosc90@gmail.com>
2023-09-21 15:47:02 +0200

    creato primo components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
+import AppTitol from "./components/AppTitol.vue";
 export default {
   data() {
     return {
       message: "Hello Vite",
     };
   },
+  components: {
+    AppTitol,
+  },
 };
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <AppTitol />
 </template>
 
 <style></style>
---
+++ src/components/AppTitol.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      titol: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ titol }}</h1>
+</template>
